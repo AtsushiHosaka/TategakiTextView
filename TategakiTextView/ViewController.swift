85c608fce2968dda478c42cbc64b7b3ae8204555
Atsushi Hosaka <hosaka2917@gmail.com>
2024-07-10 21:02:14 +0900

    Initial Commit

---
+++ TategakiTextView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TategakiTextView
+//
+//  Created by 保坂篤志 on 2024/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
